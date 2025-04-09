b6dceb93e2e5b3f2f9526895a8927191e4f1c1d0
sonyerim <119676236+sonyerim@users.noreply.github.com>
2025-04-09 15:36:26 +0900

    Create 3-4.py

---
+++ thisiscodingtest/3-4.py
@@ -0,0 +1,18 @@
+# 그리디
+# 1이 될 때까지
+
+n, k = map(int, input().split())
+count = 0
+
+while True:
+    if n == 1:
+        break
+    
+    if n % k == 0:
+        n = n // k
+        count += 1
+    else:
+        n -= 1
+        count += 1
+        
+print(count)
