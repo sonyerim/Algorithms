e750bc95b37ea5994363c1d6f5e9a2c1b9bccd52
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2025-04-23 22:20:41 +0900

    Create 7-7.py

---
+++ thisiscodingtest/binary_search/7-7.py
@@ -0,0 +1,14 @@
+# 이진 탐색
+# 부품 찾기 (set)
+
+n = int(input())
+array = set(map(int, input().split()))
+
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
