2bbfb5ab5ae7e087600ba5bf42876fb69564a557
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2025-08-25 02:11:25 +0900

    Update 1204.py

--- SWEA/1204.py
+++ SWEA/1204.py
@@ -6,17 +6,17 @@ for test_case in range(1, T + 1):
     case_num = int(input())
     score = list(map(int, input().split()))
     
-    count = [0] * 101
+    count = [0] * 101 # 배열 초기화
     answer = 0
     
     for s in score:
         count[s] += 1
         
     max_count = max(count)
-    for i in range(100, -1, -1):
+    for i in range(100, -1, -1): # 범위 설정
         if count[i] == max_count:
             answer = i
-            break
+            break # 필수
       
     print(f"#{case_num} {answer}")T = int(input())
 
