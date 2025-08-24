8a305675796ad5310e4d49ca850147174bd23889
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2025-08-25 02:10:29 +0900

    Create 1204.py

---
+++ SWEA/1204.py
@@ -0,0 +1,39 @@
+# 1204. [S/W 문제해결 기본] 1일차 - 최빈수 구하기
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    case_num = int(input())
+    score = list(map(int, input().split()))
+    
+    count = [0] * 101
+    answer = 0
+    
+    for s in score:
+        count[s] += 1
+        
+    max_count = max(count)
+    for i in range(100, -1, -1):
+        if count[i] == max_count:
+            answer = i
+            break
+      
+    print(f"#{case_num} {answer}")T = int(input())
+
+for test_case in range(1, T + 1):
+    case_num = int(input())
+    score = list(map(int, input().split()))
+    
+    count = [0] * 101
+    answer = 0
+    
+    for s in score:
+        count[s] += 1
+        
+    max_count = max(count)
+    for i in range(100, -1, -1):
+        if count[i] == max_count:
+            answer = i
+            break
+      
+    print(f"#{case_num} {answer}")
