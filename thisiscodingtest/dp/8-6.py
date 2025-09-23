09ea4d3bde86e142c0aca4e211715634fa98429f
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2025-09-24 03:08:25 +0900

    Create 8-6.py

---
+++ thisiscodingtest/dp/8-6.py
@@ -0,0 +1,16 @@
+# DP
+# 개미 전사
+
+N = int(input())
+
+K = list(map(int, input().split()))
+
+d = [0] * N
+
+d[0] = K[0]
+d[1] = max(K[0], K[1])
+
+for i in range(2, N):
+    d[i] = max(d[i - 1], d[i - 2] + K[i])
+    
+print(d[N - 1])
