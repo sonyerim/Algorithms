cf0ce7b0b5c4ce0ac04e4f7322608868ac7daa3d
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2025-10-18 02:37:50 +0900

    Update 1204.py

--- SWEA/1204.py
+++ SWEA/1204.py
@@ -18,22 +18,4 @@ for test_case in range(1, T + 1):
             answer = i
             break # 필수
       
-    print(f"#{case_num} {answer}")T = int(input())
-
-for test_case in range(1, T + 1):
-    case_num = int(input())
-    score = list(map(int, input().split()))
-    
-    count = [0] * 101
-    answer = 0
-    
-    for s in score:
-        count[s] += 1
-        
-    max_count = max(count)
-    for i in range(100, -1, -1):
-        if count[i] == max_count:
-            answer = i
-            break
-      
     print(f"#{case_num} {answer}")
