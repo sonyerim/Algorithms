793b97b06e492e05179542c94dda6e592b9da3b8
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2025-11-08 00:02:39 +0900

    Create 1992.py

---
+++ BOJ/1992.py
@@ -0,0 +1,20 @@
+# 1992번: 쿼드트리
+# 분할 정복
+# 재귀
+
+N = int(input())
+video = [list(input().strip()) for _ in range(N)]
+
+def compress(x, y, size):
+    first = video[x][y]
+
+    for i in range(x, x + size):
+        for j in range(y, y + size):
+            if video[i][j] != first:
+                half = size // 2
+                return '(' + compress(x, y, half) + compress(x, y + half, half) + compress(x + half, y, half) + compress(x + half, y + half, half) + ')'
+
+    return first
+
+
+print(compress(0, 0, N))
