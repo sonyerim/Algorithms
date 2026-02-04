8aaa63e85266c2f268723cc308a52354f99620c4
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2026-02-04 22:12:54 +0900

    Create 2563.py

---
+++ BOJ/2563.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        boolean[][] arr = new boolean[100][100];
+
+        for (int a = 0; a < N; a++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+
+            for (int i = x; i < x + 10; i++) {
+                for (int j = y; j < y + 10; j++) {
+                    arr[i][j] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j]) count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
