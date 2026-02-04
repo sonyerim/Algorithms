ebd8a68852b307302ca0be17a8f2dc43b693cb6e
Yerim Son <119676236+sonyerim@users.noreply.github.com>
2026-02-04 16:21:27 +0900

    Refactor 1210.java

--- SWEA/1210.java
+++ SWEA/1210.java
@@ -6,29 +6,35 @@ class Solution {
 	static int SIZE = 100;
 	static int[][] map = new int[SIZE][SIZE];
 	static boolean[][] visited = new boolean[SIZE][SIZE];
+	static int answer = 0;
 
-	static boolean dfs(int x, int y) {
-		if (map[x][y] == 2)
-			return true;
+	static void dfs(int x, int y) {
+		if (x == 0) {
+			answer = y;
+			return;
+		}
 
 		visited[x][y] = true;
 
 		// 왼쪽 끝까지
 		if (y > 0 && map[x][y - 1] == 1 && !visited[x][y - 1]) {
-			return dfs(x, y - 1);
+			dfs(x, y - 1);
+			return;
 		}
 
 		// 오른쪽 끝까지
 		if (y < SIZE - 1 && map[x][y + 1] == 1 && !visited[x][y + 1]) {
-			return dfs(x, y + 1);
+			dfs(x, y + 1);
+			return;
 		}
 
-		// 아래로 이동
-		if (x + 1 < SIZE && map[x + 1][y] != 0 && !visited[x + 1][y]) {
-			return dfs(x + 1, y);
+		// 위로 이동
+		if (x - 1 >= 0 && map[x - 1][y] == 1 && !visited[x - 1][y]) {
+			dfs(x - 1, y);
+			return;
 		}
 
-		return false;
+		return;
 	}
 
 	public static void main(String args[]) throws Exception {
@@ -38,7 +44,6 @@ class Solution {
 
 		for (int test_case = 1; test_case <= 10; test_case++) {
 			int tc = Integer.parseInt(br.readLine());
-			int answer = 0;
 
 			for (int i = 0; i < SIZE; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
@@ -48,14 +53,12 @@ class Solution {
 			}
 
 			for (int i = 0; i < SIZE; i++) {
-				int x = 0;
+				int x = SIZE - 1;
 				int y = i;
-				if (map[0][i] == 1) {
+				if (map[x][y] == 2) {
 					visited = new boolean[SIZE][SIZE];
-					boolean success = dfs(x, y);
-					if (success)
-						answer = i;
-
+					dfs(x, y);
+					break;
 				}
 			}
 
